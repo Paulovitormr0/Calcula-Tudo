facd7b3a3c7948d286b33b1defc5cff011fd9256
Paulovitormr0 <paulo.dev.ucb@gmail.com>
2025-08-29 19:34:37 -0300

    Atualização Contem erros

--- Testes.c
+++ Testes.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <unistd.h>
+#include <string.h>
+
 int Base1 = 0;
 int Base1AUX = 0;
 char NBase1 [20] = {0};
@@ -7,28 +9,33 @@ int NbaseAUX = 0;
 int tamanhoarray = 0;
 int Base2 = 0;
 int Ncasas = 0;
+int voltas = 0;
+
 int main(){
 
-  printf("Digite a base do primeiro numero:");
+  printf("Digite a base do primeiro numero:\n");
   scanf("%d", Base1);
-  printf("Digite o primeirou numero");
-  scanf("%d", NBase1);
-  printf("Digite a base de destino");
+  printf("Digite o primeirou numero\n");
+  scanf("%s", NBase1);
+  printf("Digite a base de destino\n");
   scanf("%d", Base2);
 
   for(Ncasas = 0; NBase1 != 0; Ncasas++){
-    NbaseAUX = NBase1;
+    Ncasas = strlen(NBase1);
     NbaseAUX = NbaseAUX / 10;
     printf("%d", Ncasas);
     sleep(2);
      
-  for(i = 0; i == Ncasas; i++){
+  for(int a = 0; a == Ncasas; a++){
     if(Ncasas = 0){
       Base1 = 1;
     }else{
     Base1AUX = Base1*Base1;
-    for (i = 0; i < tamanhoarray; i++){
-       Base1AUX * 
+    for(int i = 0; i < tamanhoarray; i++){
+      while(voltas != tamanhoarray){
+        Base1AUX = (Base1AUX*Base1AUX);
+        voltas++;
+      }
     }
     }
   }
