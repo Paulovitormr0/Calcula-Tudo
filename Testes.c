681121e829ec2876f5fc2143c12e619baf3892fa
Paulovitormr0 <paulo.dev.ucb@gmail.com>
2025-08-30 12:24:28 -0300

    Atualização

--- Testes.c
+++ Testes.c
@@ -10,15 +10,18 @@ int tamanhoarray = 0;
 int Base2 = 0;
 int Ncasas = 0;
 int voltas = 0;
+int Base1AUX = 0;
 
 int main(){
 
   printf("Digite a base do primeiro numero:\n");
-  scanf("%d", Base1);
+  scanf("algo %d", &Base1);
   printf("Digite o primeirou numero\n");
+  fflush(stdin);
   scanf("%s", NBase1);
   printf("Digite a base de destino\n");
-  scanf("%d", Base2);
+  fflush(stdin);
+  scanf("%d", &Base2);
 
   for(Ncasas = 0; NBase1 != 0; Ncasas++){
     Ncasas = strlen(NBase1);
@@ -27,13 +30,14 @@ int main(){
     sleep(2);
      
   for(int a = 0; a == Ncasas; a++){
-    if(Ncasas = 0){
+    if(Ncasas == 0){
       Base1 = 1;
     }else{
-    Base1AUX = Base1*Base1;
     for(int i = 0; i < tamanhoarray; i++){
       while(voltas != tamanhoarray){
         Base1AUX = (Base1AUX*Base1AUX);
+        Base1AUX*
+        printf("%d", Base1AUX);
         voltas++;
       }
     }
